66a6867b525f44f219bfdb2cfe321e9e8836ab10
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2021-11-28 16:44:44 -0500

    Fix tests

--- core-tests/shared/src/test/scala-3/zio/meta/TypeMonikerMacrosSpec.scala
+++ core-tests/shared/src/test/scala-3/zio/meta/TypeMonikerMacrosSpec.scala
@@ -4,13 +4,14 @@ object TypeMonikerMacrosSpec extends zio.test.DefaultRunnableSpec:
   def spec = suite("MonikerMacros Spec")(
     test("It should support getting the moniker for a kind 0 case class"){
       val moniker = TypeMonikerMacros.moniker[Person]
-      assertTrue(moniker != null) && assertTrue(moniker.name == "zio.meta.TypeMonikerMacrosSpec$Person")
+      assertTrue(moniker != null) && assertTrue(moniker.name == "zio.meta.TypeMonikerMacrosSpec$.Person")
     } + test("It should support getting the moniker for a parametic trait"){
       val moniker = TypeMonikerMacros.moniker[Func[Int,String]]
-      assertTrue(moniker != null) && assertTrue(moniker.name == "zio.meta.TypeMonikerMacrosSpec$Func[int, java.lang.String]")
+      assertTrue(moniker != null) && assertTrue(moniker.name == "zio.meta.TypeMonikerMacrosSpec$.Func[scala.Int, java.lang.String]")
     }+ test("It should support getting the moniker for a parametic trait"){
-      val moniker = TypeMonikerMacros.moniker[Func[Int,?]] //
-      assertTrue(moniker != null) && assertTrue(moniker.name == "zio.meta.TypeMonikerMacrosSpec$Func[int, java.lang.String]")
+      //TODO: See if we can make this print a wildcard symbol instead of scala.Nothing
+      val moniker = TypeMonikerMacros.moniker[Func[Int,?]]
+      assertTrue(moniker != null) && assertTrue(moniker.name == "zio.meta.TypeMonikerMacrosSpec$.Func[scala.Int, scala.Nothing]")
     }
   )
 
